65d4fa273059e3db134f72900421689ed2775139
xiechuangfan <xie.chuangfan@ztcholdings.com>
2021-09-23 20:21:52 +0800

    第一次提交

---
+++ src/views/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" :src="img">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+  name: 'Home',
+
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      img: require('@/assets/logo.png')
+    }
+  },
+  created(){
+    console.log('process.env.NODE_ENV :>> ', process.env.VUE_APP_BASE_API)
+  }
+}
+</script>
---
+++ vue.config.js
@@ -0,0 +1,41 @@
+'use strict'
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const name = 'vue Template' // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following methods:
+// port = 9528 npm run dev OR npm run dev --port = 9528
+const port = process.env.port || process.env.npm_config_port || 9528 // dev port
+console.log('process.env.NODE_ENV :>> ', process.env.NODE_ENV)
+module.exports = {
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  }
+}
